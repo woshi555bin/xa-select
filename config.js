c53a2be6be2fcc866f56f91ed346c588be410361
wangbin31 <wangbin31@58.com>
2019-11-06 13:41:44 +0800

    测试cli

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports=[
+    {
+        type:"confirm",
+        name:"private",
+        message:"this resgistery is private?"
+    },
+    {
+        type:"input",
+        name:"author",
+        message:"author?"
+    },
+    {
+        type:"input",
+        name:"description",
+        message:"description?" 
+    }
+]
